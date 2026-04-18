f0d1342599a89b98ec0dbdaf3931ba2d28e36fe3
itzmeshaik <hishamshaik23@gmail.com>
2026-04-18 12:14:27 -0400

    Added extra functionality to app.py

--- app.py
+++ app.py
@@ -3,19 +3,29 @@ import yt_dlp
 url = input("enter the full url of the video you want to download: ")
 
 type = input("do you only want to download the audio of the video(y/n)?:")
+try:
+    if type.lower == "y":
+        ydl_opts = {
+            "format": "bestaudio/best",
+            "postprocessors": [{"key": "FFmpegExtractAudio"}],
+        }
+    else:
+        ydl_opts = {
+            "format": "bestaudio+bestvideo",
+            "merge_output_format": "mp4",
+        }
 
-if type.lower == 'y':
-    ydl_opts = {
-            'format':'bestaudio/best',
-            'postprocessors': [{
-                'key':'FFmpegExtractAudio'
-                }],
-    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(url)
+
+except yt_dlp.utils.DownloadError:
+    print("there was an error in downloading the video. Try Again")
+
+except KeyboardInterrupt:
+    print("Bye!👋")
+
 else:
-    ydl_opts = {
-            'format': 'bestaudio+bestvideo',
-            'merge_output_format': 'mp4',
-    }
+    print("downloading successful.")
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(url)
+finally:
+    print("execution complete.")
